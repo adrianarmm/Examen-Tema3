080be4bc81656014bb17f3611dda16fcf65c4a82
adrianarmm <areyemor@myuax.com>
2023-12-05 11:34:16 +0100

    47

--- Gestión de Errores.cpp
+++ Gestión de Errores.cpp
@@ -35,7 +35,7 @@ void Environment::insert(const std::string& symbol, int value) {
     if (it!= symbolTable.end()) {
 throw std::runtime_error("Undefined symbol: " + symbol + " ya esta definido, ademas existe con otro valor")
     }
-
+std::cout << "El simbolo " << symbol << " ha sido definido correctamente :)" << std::endl;
 
 
 }
