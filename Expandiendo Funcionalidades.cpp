318d3f2968cb266c5e7c59c4581857938cabd99d
adrianarmm <areyemor@myuax.com>
2023-12-05 12:25:33 +0100

    106

--- Expandiendo Funcionalidades.cpp
+++ Expandiendo Funcionalidades.cpp
@@ -4,3 +4,12 @@
 #include <iostream>
 #include <variant>
 
+class Environment {
+public:
+    Environment();
+    void define(const std::string& symbol, const std::variant<int, double, std::string>& value);
+    std::variant<int, double, std::string> lookup(const std::string& symbol) const;
+    bool isDefined(const std::string& symbol) const;
+    void remove(const std::string& symbol);
+    template<typename T>
+    bool hasType(const std::string& symbol) const;
