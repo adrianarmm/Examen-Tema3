e6f5baa17b0e6b1b8269f3a328463ea902deee23
adrianarmm <areyemor@myuax.com>
2023-12-05 11:43:04 +0100

    57

--- Gestión de Errores.cpp
+++ Gestión de Errores.cpp
@@ -30,7 +30,7 @@ int Environment::lookup(const std::string& symbol) const {
 bool Environment::isDefined(const std::string& symbol) const {
     return symbolTable.find(symbol) != symbolTable.end();
 }
-void Environment::insert(const std::string& symbol, int value) {
+void Environment::insert(const std::string& symbol, int value)  {
     auto it = symbolTable.find(symbol);
     if (it!= symbolTable.end()) {
     throw std::runtime_error("Undefined symbol: " + symbol + " ya esta definido, ademas existe con otro valor");;
@@ -46,16 +46,4 @@ int main () {
     env.insert("x", 2);
     env.insert("y", 3);
     env.insert("z", 4);
-
-    std::cout << "x = " << env.lookup("x") << std::endl;
-    std::cout << "y = " << env.lookup("y") << std::endl;
-    std::cout << "z = " << env.lookup("z") << std::endl;
-
-        try {
-        std::cout << "w = " << env.lookup("w") << std::endl;
-    } catch (const std::runtime_error& e) {
-        std::cout << "Error: " << e.what() << std::endl;
     }
-    return 0;
-
-}
