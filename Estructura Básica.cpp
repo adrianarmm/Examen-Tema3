e7adda81083e1d642bbbdb0d61470bf1ccee6212
adrianarmm <areyemor@myuax.com>
2023-12-05 10:50:18 +0100

    13

--- Estructura Básica.cpp
+++ Estructura Básica.cpp
@@ -16,3 +16,9 @@ Environment::Environment() {}
 void Environment::define(const std::string& symbol, int value) {
     symbolTable[symbol] = value;
 }
+int Environment::lookup(const std::string& symbol) const {
+    auto it = symbolTable.find(symbol);
+}
+if (it == symbolTable.end()) {
+    throw std::runtime_error("Undefined symbol: " + symbol);
+         }
