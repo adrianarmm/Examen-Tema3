6cc26f9c9fca15db1f013a08ab0a62f4d08355a3
adrianarmm <areyemor@myuax.com>
2023-12-05 10:44:33 +0100

    6

--- Estructura Básica.cpp
+++ Estructura Básica.cpp
@@ -2,7 +2,8 @@
 #include <string>
 
 class Environment {
-public:Environment();
+public:
+    Environment();
     void set(const std::string& symbol, int value);
     int get(const std::string& symbol) const;
 };
