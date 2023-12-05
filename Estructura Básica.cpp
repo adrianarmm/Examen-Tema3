b24d45e765b078ee1b027dde948ecb75f77f6ba0
adrianarmm <areyemor@myuax.com>
2023-12-05 11:12:42 +0100

    24

--- Estructura Básica.cpp
+++ Estructura Básica.cpp
@@ -8,6 +8,7 @@ public:
     void define(const std::string& symbol, int value);
     int lookup(const std::string& symbol) const;
 bool isDefined(const std::string& symbol) const;
+
 private:
     std::map<std::string, int> symbolTable;
 };
@@ -16,12 +17,13 @@ Environment::Environment() {}
 void Environment::define(const std::string& symbol, int value) {
     symbolTable[symbol] = value;
 }
-int Environment::lookup(const std::string& symbol) const {
-    auto it = symbolTable.find(symbol);
-if (it == symbolTable.end()) {
-    throw std::runtime_error("Undefined symbol: " + symbol);
-         }
-return it->second;
+
+    int Environment::lookup(const std::string& symbol) const {
+        auto it = symbolTable.find(symbol);
+        if (it == symbolTable.end()) {
+            throw std::runtime_error("Undefined symbol: " + symbol);
+        }
+    return it->second;
 }
 
 bool Environment::isDefined(const std::string& symbol) const {
@@ -32,13 +34,9 @@ int main () {
   env.define("x", 2);
   env.define("y", 3);
   env.define("z", 4);
-  std::cout << env.lookup("x") << std::endl;
-  std::cout << env.lookup("y") << std::endl;
-  std::cout << env.lookup("z") << std::endl;
-  std::cout << env.lookup("w") << std::endl;
-  try {
-    std::cout << env.lookup("w") << std::endl;
-  } catch (std::runtime_error& e) {
-    std::cout << e.what() << std::endl;
-  }
+
+    std::cout << "x = " << env.lookup("x") << std::endl;
+    std::cout << "y = " << env.lookup("y") << std::endl;
+    std::cout << "z = " << env.lookup("z") << std::endl;
+    return 0;
 }
